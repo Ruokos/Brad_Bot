58ca44a8f59bd448f18dfb773361471909462ce6
Tristan <Tristanjanmaat@gmail.com>
2023-03-17 15:59:28 +0100

    Brad kan hierin een bericht in de general sturen zodra hij online komt

--- brad.py
+++ brad.py
@@ -1,16 +1,28 @@
 import discord
 
 from dotenv import load_dotenv
+from discord.ext import commands
+COMMAND_PREFIX = '##'
+CHANNEL_ID = 690107427663904905 #Algemeen van de BRUHHHHH discord server
+
+
 
 intents = discord.Intents.default()
 
 load_dotenv()
 TOKEN = "MTA4NjI4MzIxNTgxMzIxODM3NQ.Gln0Yn.xght6qFSImPqEe0idBJJiHgX7dAVz4-OfyvHFE" 
 
-client = discord.Client(intents=intents)
-    
-@client.event
-async def on_ready():
-    print(f'{client.user} is verbonden met Discord!')
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
-client.run(TOKEN)
+@bot.event
+async def on_ready():
+    print("Hallo, Brad is hier!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Hallo, Brad is hier!")
+
+@bot.command()
+async def send(ctx, *, message: str):
+    await ctx.send(message)
+
+
+bot.run(TOKEN)
