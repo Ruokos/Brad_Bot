ea1ecbcb4df889c0891c6d2aca80473df2994400
Tristan <Tristanjanmaat@gmail.com>
2023-03-17 16:42:36 +0100

    Eerste command die daadwerkelijk werkt

--- brad.py
+++ brad.py
@@ -4,11 +4,12 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 
-COMMAND_PREFIX = '##'
+COMMAND_PREFIX = '!'
 CHANNEL_ID = 690107427663904905 #Algemeen van de BRUHHHHH discord server
 load_dotenv() #Laad de token van de BOT in
 TOKEN = os.environ["TOKEN"]
-intents = discord.Intents.default()
+intents = discord.Intents.all()
+
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
@@ -16,11 +17,11 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 async def on_ready():
     print("Hallo, Brad is hier!")
     channel = bot.get_channel(CHANNEL_ID)
-    #await channel.send("Hallo, Brad is hier!")
+    await channel.send("Hallo, Brad is hier!")
 
 @bot.command()
-async def send(ctx, *, message: str):
-    await ctx.send(message)
+async def brad(ctx):
+    await ctx.send("Ik ben Brad")
 
 
 bot.run(TOKEN)
