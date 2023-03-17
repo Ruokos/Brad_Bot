96ec6ed9488da6380dffaeff6732a00261a3e332
TsarBombastic <mvdmds1@hotmail.com>
2023-03-17 15:16:14 +0100

    changed bot token

--- brad.py
+++ brad.py
@@ -3,13 +3,13 @@ from discord.ext import commands
 
 def main():
 
-    bot = commands.Bot(command_prefix='!', token='MTA4NjI4MzIxNTgxMzIxODM3NQ.Gn7ywL.eb2-Fxey3J3D0sFFgG1uLs2j5MY7rEZcx9LP0s')
+    bot = commands.Bot(command_prefix='!', token='MTA4NjI4MzIxNTgxMzIxODM3NQ.Gln0Yn.xght6qFSImPqEe0idBJJiHgX7dAVz4-OfyvHFE')
 
     @bot.command()
     async def hello(ctx):
         await ctx.send('Hello!')
 
-    bot.run('MTA4NjI4MzIxNTgxMzIxODM3NQ.Gn7ywL.eb2-Fxey3J3D0sFFgG1uLs2j5MY7rEZcx9LP0s')
+    bot.run('MTA4NjI4MzIxNTgxMzIxODM3NQ.Gln0Yn.xght6qFSImPqEe0idBJJiHgX7dAVz4-OfyvHFE')
 
 if __name__ == "__main__":
     main()
